1efd058cd13884a0647847bc68ae0fe37ce38873
Aomi-Kobayashi <aaaaomi@gmail.com>
2023-09-07 14:17:53 +0900

    build

---
+++ if-sample/if.go
@@ -0,0 +1,19 @@
+package if-sample
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// ランダム生成された1~10の整数で今日の運勢を占う
+func ifToday() {
+
+	var num int = (rand.Intn(10))
+	fmt.Println(num)
+
+	if num == 7 || num == 9 {
+		fmt.Println("Today is GodDay!")
+	} else {
+		fmt.Println("Well,it's Ok")
+	}
+}
---
+++ switch-sample/switch.go
@@ -0,0 +1,29 @@
+package switch-sample
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// 現在時刻を取得し時刻に合わせて挨拶をする
+func switchHour() {
+	// 「13:00」と出力したくstringをintに変換したがiが出力されない
+	var s string = ":00"
+	i, _ := strconv.Atoi(s)
+
+	t := time.Now().Hour()
+
+	fmt.Println(t + i)
+
+	switch {
+	case t < 12:
+		fmt.Println("Good Morning")
+	case t < 17:
+		fmt.Println("Good Aftrnoon")
+	case t == 22 || t == 23:
+		fmt.Println("Good Night")
+	default:
+		fmt.Println("Good Evening")
+	}
+}
