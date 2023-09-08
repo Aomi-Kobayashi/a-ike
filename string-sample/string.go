69162ff9e87e6e94c20dc4f6a54ace4598ae47db
Aomi-Kobayashi <aaaaomi@gmail.com>
2023-09-08 11:14:59 +0900

    new & add

---
+++ string-sample/string.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	// 結合
+	s := "VALORANT"
+	s = s + "+ APEX"
+	fmt.Println(s)
+
+	// 大文字、小文字変換
+	s = "ValorantApex"
+	fmt.Println(strings.ToUpper(s))
+	fmt.Println(strings.ToLower(s))
+
+	// 部分取得
+	s = "VALORANTApex"
+	fmt.Println(s[0:9])
+	fmt.Println(s[9:])
+	fmt.Println(s[:9])
+	fmt.Println(s[:])
+
+	// 両端トリム
+	s = "     VALORANT    "
+	fmt.Printf("[%s]/n", strings.TrimSpace(s))
+
+	// トリム対象指定
+	s = "     VALORANT    "
+	fmt.Printf("[%s]/n", strings.Trim(s, " "))
+
+	// 左、右トリム
+	s = "     VALORANT     "
+	fmt.Printf("[%s]\n", strings.TrimLeft(s, " "))
+
+	s = "     VALORANT     "
+	fmt.Printf("[%s]\n", strings.TrimRight(s, " "))
+
+	// 指定文字が含まれているか
+	s = "Apple Bear Cookie Dog Earth Fox Git"
+	fmt.Println(strings.Contains(s, "Cookie"))
+	fmt.Println(strings.Contains(s, "Happy"))
+
+	// 指定文字列の出現位置
+	s = "Apple Bear Cookie Dog Earth Fox Git"
+	fmt.Println(strings.Index(s, "Cookie"))
+	fmt.Println(strings.Index(s, "Happy"))
+
+	// 指定文字列の最後の出現位置
+	s = "Apple Bear Cookie Dog Earth Fox Git"
+	fmt.Println(strings.LastIndex(s, "o"))
+
+	// 先頭一致
+	s = "Apple Bear Cookie Dog Earth Fox Git"
+	fmt.Println(strings.HasPrefix(s, "Aplle"))
+
+	// 後方一致
+	s = "Apple Bear Cookie Dog Earth Fox Git"
+	fmt.Println(strings.HasSuffix(s, "Git"))
+
+	// 文字列変換
+	s = "bookbookbookbook"
+	fmt.Println(strings.Replace(s, "oo", "uu", 3))
+	fmt.Println(strings.Replace(s, "oo", "ee", -1))
+
+	// 繰り返し
+	s = "apex"
+	fmt.Println(strings.Repeat(s, 4))
+
+	// タイトルケースへの変換
+	s = "apple bear cookie dog earth fox git"
+	fmt.Println(strings.Title(s))
+
+	// 分割
+	s = "Apple Bear Cookie Dog Earth Fox Git"
+	slice := strings.Split(s, "")
+	for _, str := range slice {
+		fmt.Printf("[%s]", str)
+	}
+	fmt.Println("")
+
+	// 結合2
+	slice = []string{"Apple", "Bear", "Cookie", "Dog", "Earth", "Fox", "Git"}
+	fmt.Println(strings.Join(slice, ","))
+
+	// 文字列長カウント
+	s = "Apple Bear Cookie Dog Earth Fox Git"
+	fmt.Println(len(s))
+
+	// 文字列長カウント(マルチバイト)
+	s = "Java Go言語"
+	fmt.Println(len(s))
+	fmt.Println(len([]rune(s)))
+
+	// 複数行文字列
+	s = `
+	This is a sample.
+	golang multiple lines.
+	`
+	fmt.Println(s)
+
+	// 文字列→浮動小数点変換
+	s = "990.339"
+	sf, _ := strconv.ParseFloat(s, 64)
+	sf = sf + 0.1
+	fmt.Println(sf)
+}
